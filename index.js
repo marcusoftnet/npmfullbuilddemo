b412788a2ad7b926d851ddb0270b57a0d278e1a0
Marcus Hammarberg <marcusoft.net@gmail.com>
2015-08-24 15:26:53 +0700

    First passing version of typescript

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var fill = require("./lib/coffeeCode.js");
+var greeter = require("./lib/tsCode.js");
 
-module.exports.fillingCoffee = function () {
-	return fill("Jar", "Java");
+module.exports.fillMyJar = function (beverage) {
+	return fill("jar", beverage);
+};
+
+module.exports.greetAType = function (name) {
+	var g = new greeter();
+	return g.greet(name);
 };
--- test/test.js
+++ test/test.js
@@ -9,7 +9,12 @@ describe("Test placeholder", function () {
 	});
 
 	it("calls into stuff written in coffeescript", function (done) {
-		app.fillingCoffee().should.equal("Filling the Jar with Java...");
+		app.fillMyJar("Java").should.equal("Filling the jar with Java...");
+		done();
+	});
+
+	it("calls into stuff written in typescript too", function (done) {
+		app.greetAType("Marcus").should.equal("A type-scripting greeting to you, Marcus");
 		done();
 	});
 });
