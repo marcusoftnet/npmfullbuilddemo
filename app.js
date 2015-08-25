e9f102e5b8d31d79360ebd8f300c86403881a6a2
Marcus Hammarberg <marcusoft.net@gmail.com>
2015-08-25 10:09:34 +0700

    Small update, ready to be deployed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var koa = require('koa');
 var app = koa();
 
 app.use(function *(){
-  this.body = 'Hello World';
+  this.body = 'Deployed with a build script using npm only, baby!';
 });
 
 app.listen(process.env.PORT || 3000);
