631b27217beaefa16d3a5849193abbd0d88a5200
Marcus Hammarberg <marcusoft.net@gmail.com>
2015-08-26 11:09:13 +0700

    Stuff added after latest blog post

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 var koa = require('koa');
 var app = koa();
 
+
+	console.log(process.argv[2]);
+
+var htmlCode = '<html> \
+	<head> \
+		<title>A title</title> \
+		 \
+		<!-- Enable live-reloading --> \
+		<script src="//localhost:9091"></script> \
+	</head> \
+	<body> \
+		<h1>Deployed with a build script using npm only, baby!</h1> \
+		And live-reloading added too... \
+	</body> \
+</html>';
+
 app.use(function *(){
-  this.body = 'Deployed with a build script using npm only, baby!';
+	this.body = htmlCode;
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || (process.argv[2] || 3000));
